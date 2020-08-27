02c40648287f2af951ce90ad471cda4b4abd9a2d
JeshurunLuke <jeshu@live.unc.edu>
2020-08-27 17:30:10 -0400

    Easier Setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install make
+sudo apt install cmake
+sudo apt install mpirun
+sudo apt install python3
+sudo apt install git
+python3 complete_install.py --lammps $PWD
+sudo apt install python3-pip
+source ~/.bashrc
+pip3 install numpy
+pip3 install random
+pip3 install pytest
+pip3 install pandas
